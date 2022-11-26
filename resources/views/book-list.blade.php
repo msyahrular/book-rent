2f68c8becedf43358de81d5c74681780286d7175
msyahrular <riki.valintine@gmail.com>
2022-11-27 05:00:52 +0700

    Membuat fitur pencarian

--- resources/views/book-list.blade.php
+++ resources/views/book-list.blade.php
@@ -3,6 +3,26 @@
 @section('tittle', 'Book List')
 
 @section('content')
+
+    <form action="" method="get">
+        <div class="row">
+            <div class="col-12 col-sm-6">
+                <select name="category" id="category" class="form-control">
+                    <option value="">Select Category</option>
+                    @foreach ($categories as $item)
+                        <option value="{{$item->id}}">{{$item->name}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="col-12 col-sm-6">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" name="title" placeholder="Search Book's Title" aria-label="Search Book's Title">
+                    <button class="btn btn-primary" type="submit">Search</button>
+                </div>
+            </div>
+        </div>
+    </form>
+
     
     <div class="my-5">
         <div class="row">
